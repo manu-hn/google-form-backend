ea18ec8ce9cd11014529cf0ada4090e23042f3ef
manu-hn <manuu.hn11@gmail.com>
2024-06-18 08:51:11 +0530

    Routes Implemented

--- src/routes/GoogleForm.routes.js
+++ src/routes/GoogleForm.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
+require('module-alias/register');
+const { submitGoogleFormDetails } = require("@controller/GoogleForm.controller.js");
 
 const router = express.Router();
 
 
-router.post('/submit');
+router.post('/submit', submitGoogleFormDetails);
 
 
 module.exports = router;
